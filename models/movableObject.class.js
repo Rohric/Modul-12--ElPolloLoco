7776a71a4de65b9a94888c0a01da0fcb5dbdab69
Rohric <emilmarsal@icloud.com>
2025-10-14 19:56:35 +0200

    add new function to load all images for animations

--- models/movableObject.class.js
+++ models/movableObject.class.js
@@ -2,12 +2,21 @@ class MovableObject {
   x = 120;
   y = 280;
   img;
-  width= 100;
-  height=150;
+  width = 100;
+  height = 150;
+  imageCache = [];
 
   loadImage(path) {
     this.img = new Image();
-    this.img.src=path;
+    this.img.src = path;
+  }
+
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = path;
+    });
   }
 
   moveRight() {
