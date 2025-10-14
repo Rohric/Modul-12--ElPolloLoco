b266a7bbd3200c029e89485691c0dbdfe5ac14ff
Rohric <emilmarsal@icloud.com>
2025-10-14 19:27:52 +0200

    add backgrounds

--- models/world.class.js
+++ models/world.class.js
@@ -3,7 +3,10 @@ class World {
   enemies = [new chicken(), new chicken(), new chicken()];
   clouds = [new Cloud()];
   backgroundObjects = [
+    new BackgroundObject("img/5_background/layers/air.png", 0),
     new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
+    new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
+    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
   ];
   canvas;
   ctx;
@@ -18,7 +21,7 @@ class World {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
     this.addObjectsToMap(this.backgroundObjects);
-    
+
     this.addToMap(this.character);
     this.addObjectsToMap(this.enemies);
     this.addObjectsToMap(this.clouds);
