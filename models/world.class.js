9ac0adca2e6a1fd6b8a45cce4f4e3451bf02daa0
Rohric <emilmarsal@icloud.com>
2025-10-15 00:42:53 +0200

    add camera movement

--- models/world.class.js
+++ models/world.class.js
@@ -9,8 +9,9 @@ class World {
     new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
   ];
   canvas;
-  keyboard;
   ctx;
+  keyboard;
+  camera_x = -0;
 
   constructor(canvas) {
     this.ctx = canvas.getContext("2d");
@@ -27,11 +28,15 @@ class World {
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+    this.ctx.translate(this.camera_x,0);
+
     this.addObjectsToMap(this.backgroundObjects);
 
     this.addToMap(this.character);
     this.addObjectsToMap(this.enemies);
     this.addObjectsToMap(this.clouds);
+
+    this.ctx.translate(-this.camera_x,0);
 
     let self = this;
     requestAnimationFrame(function () {
