3da12234b9eaf21bcfc64a9fda2d282bc6d1421e
Rohric <emilmarsal@icloud.com>
2025-10-14 20:28:07 +0200

    add animation to chicken

--- models/chicken.class..js
+++ models/chicken.class..js
@@ -2,10 +2,30 @@ class Chicken extends MovableObject {
   y = 370;
   height = 60;
   width = 80;
+  images_walking = [
+    "img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+    "img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
+    "img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
+  ];
 
   constructor() {
     super().loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+    this.loadImages(this.images_walking);
 
     this.x = 200 + Math.random() * 500;
+    this.speed = 0.15 + Math.random() * 0.5;
+
+    this.animate();
+  }
+
+  animate() {
+    this.moveLeft();
+
+    setInterval(() => {
+      let intervall = this.currentImage % this.images_walking.length;
+      let path = this.images_walking[intervall];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 100);
   }
 }
--- models/movableObject.class.js
+++ models/movableObject.class.js
@@ -5,6 +5,9 @@ class MovableObject {
   width = 100;
   height = 150;
   imageCache = [];
+  currentImage = 0;
+  speed = 0.15;
+
 
   loadImage(path) {
     this.img = new Image();
@@ -15,7 +18,7 @@ class MovableObject {
     arr.forEach((path) => {
       let img = new Image();
       img.src = path;
-      this.imageCache[path] = path;
+      this.imageCache[path] = img;
     });
   }
 
@@ -23,6 +26,8 @@ class MovableObject {
     console.log("Moving right");
   }
   moveLeft() {
-    console.log("Moving left");
+    setInterval(() => {
+      this.x -= this.speed;
+    }, 1000 / 60);
   }
 }
