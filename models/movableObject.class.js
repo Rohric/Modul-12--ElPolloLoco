886a2f183321ad2da681b094e0dc6a10a7f83ccb
Rohric <emilmarsal@icloud.com>
2025-10-15 12:48:23 +0200

    add isAboveGround Function for Jumping and other animations

--- models/movableObject.class.js
+++ models/movableObject.class.js
@@ -8,7 +8,23 @@ class MovableObject {
   currentImage = 0;
   speed = 0.15;
 
+  speedY = 0;
+  acceleration = 2.5;
+
   otherDirection = false;
+
+  applyGravity() {
+    setInterval(() => {
+      if (this.isAboveGround()) {
+        this.y -= this.speedY;
+        this.speedY -= this.acceleration;
+      }
+    }, 1000 / 25);
+  }
+
+  isAboveGround() {
+    return this.y < 180;
+  }
 
   loadImage(path) {
     this.img = new Image();
