60869f215bb50783f4fa6c7bbf2946880fa2a7eb
Rohric <emilmarsal@icloud.com>
2025-10-21 16:41:57 +0200

    add StartScreen

--- java_script/game.js
+++ java_script/game.js
@@ -1,62 +1,87 @@
 let canvas;
 let world;
-let keyboard = new Keyboard();
+const keyboard = new Keyboard();
+let gameStarted = false;
 
-function init() {
+function setupGame() {
   canvas = document.getElementById("canvas");
-  world = new World(canvas, keyboard);
-
-  console.log("My Charakter is", world.character);
+  registerKeyboardEvents();
 }
 
-window.addEventListener("keydown", (event) => {
-  if (event.keyCode == 32) {
-    keyboard.SPACE = true;
+function startGame() {
+  if (gameStarted) {
+    return;
   }
+  gameStarted = true;
+  showGameScreen();
+  world = new World(canvas, keyboard);
+}
 
-   if (event.keyCode == 37) {
-    keyboard.LEFT = true;
+function showGameScreen() {
+  const startScreen = document.getElementById("Startscreen");
+  const gameContainer = document.getElementById("gameContainer");
+  if (startScreen) {
+    startScreen.classList.add("display_none");
   }
+  if (gameContainer) {
+    gameContainer.classList.remove("display_none");
+  }
+}
 
-   if (event.keyCode == 38) {
-    keyboard.UP = true;
-  }
+function registerKeyboardEvents() {
+  window.addEventListener("keydown", handleKeyDown);
+  window.addEventListener("keyup", handleKeyUp);
+}
 
-   if (event.keyCode == 39) {
-    keyboard.RIGHT = true;
+function handleKeyDown(event) {
+  switch (event.keyCode) {
+    case 32:
+      keyboard.SPACE = true;
+      break;
+    case 37:
+      keyboard.LEFT = true;
+      break;
+    case 38:
+      keyboard.UP = true;
+      break;
+    case 39:
+      keyboard.RIGHT = true;
+      break;
+    case 40:
+      keyboard.DOWN = true;
+      break;
+    case 68:
+      keyboard.D = true;
+      break;
+    case 80:
+      keyboard.P = true;
+      startGame();
+      break;
   }
+}
 
-   if (event.keyCode == 40) {
-    keyboard.DOWN = true;
+function handleKeyUp(event) {
+  switch (event.keyCode) {
+    case 32:
+      keyboard.SPACE = false;
+      break;
+    case 37:
+      keyboard.LEFT = false;
+      break;
+    case 38:
+      keyboard.UP = false;
+      break;
+    case 39:
+      keyboard.RIGHT = false;
+      break;
+    case 40:
+      keyboard.DOWN = false;
+      break;
+    case 68:
+      keyboard.D = false;
+      break;
+    case 80:
+      keyboard.P = false;
+      break;
   }
-
-   if (event.keyCode == 68) {
-    keyboard.D = true;
-  }
-});
-
-window.addEventListener("keyup", (event) => {
-  if (event.keyCode == 32) {
-    keyboard.SPACE = false;
-  }
-
-   if (event.keyCode == 37) {
-    keyboard.LEFT = false;
-  }
-
-   if (event.keyCode == 38) {
-    keyboard.UP = false;
-  }
-
-   if (event.keyCode == 39) {
-    keyboard.RIGHT = false;
-  }
-
-   if (event.keyCode == 40) {
-    keyboard.DOWN = false;
-  }
-
-    if (event.keyCode == 68) {
-    keyboard.D = false;
-  }
-});
+}
